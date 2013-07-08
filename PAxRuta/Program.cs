cc1b7c1755a8537380b65191096556092c1bfcfc
peterax <ducduy8585@hotmail.com>
2013-07-08 16:20:10 +0700

    commit with PBI12 #12

--- PAxRuta/Program.cs
+++ PAxRuta/Program.cs
@@ -17,7 +17,7 @@ namespace PAxRuta
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // change somethings 2
+            // change somethings 23
         }
     }
 }
