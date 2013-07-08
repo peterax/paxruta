5cda780ac0541e760bb7369273d536b8b31fb0cf
peterax <ducduy8585@hotmail.com>
2013-07-08 16:13:14 +0700

    test link PBI12 #12

--- PAxRuta/Program.cs
+++ PAxRuta/Program.cs
@@ -17,7 +17,7 @@ namespace PAxRuta
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // change somethings
+            // change somethings 2
         }
     }
 }
